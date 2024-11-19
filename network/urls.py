92679085da66180a8ec05affae12860f115257ba
Nagy Noel <nagy.noel@students.jedlik.eu>
2024-11-19 08:41:28 +0100

    CSS működésének helyreállítása

--- network/urls.py
+++ network/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index_view, name="index"),
     path('connect/<str:ip_address>/', views.connect, name="connect"),
     path('send_command/', views.send_command, name="send_command")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- network_interface/settings.py
+++ network_interface/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,13 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFIELS_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'network/static'),]
